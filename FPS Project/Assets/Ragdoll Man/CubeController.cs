68dee27ebec952b78f4395f2708119d98d32cba8
Morgan Skillicorn <hello@morgan.games>
2020-10-02 02:42:20 +0100

    week 2 push

---
+++ FPS Project/Assets/Ragdoll Man/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    [SerializeField] private float explosionForce = 500.0f;
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 explosionPos = Vector3.zero;
+
+            rigidBody.AddExplosionForce(explosionForce, explosionPos, 100);
+        }
+    }
+}
